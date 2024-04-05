92b4073fe6597f7ab31b91b63b7b3a0321567ec1
Vinicius Alves Arcanjo <vinicius.vini041@gmail.com>
2024-04-05 04:55:21 -0300

    refac: visual

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
