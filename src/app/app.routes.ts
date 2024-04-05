a2ce0e2bf25af14fb9fa5eedeaf1276cd920d108
Vinicius Alves Arcanjo <vinicius.vini041@gmail.com>
2024-04-04 21:17:59 -0300

    feature: adicionando página de registro e detalhes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'produtos', component: ProdutosComponent },
+    { path: 'registro', component: RegistroComponent },
+    { path: 'detalhes', component: DetalhesComponent },
 ];
---
+++ src/app/pages/detalhes/detalhes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalhes.component.html',
+  styleUrl: './detalhes.component.css'
+})
+export class DetalhesComponent {
+
+}
