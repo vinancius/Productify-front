79ab637afd4bf494d33d3e838ff3b2fc83a41ada
Vinicius Alves Arcanjo <vinicius.vini041@gmail.com>
2024-04-04 21:19:28 -0300

    feature: novos campos na tabela, adicionando atributos na paginação, adicionando ações na tabela

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+import { ProdutosServiceService } from '../../service/produtosService/produtos-service.service';
+import { ListaPaginada } from '../../models/listaPaginada.model';
+import { DatePipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { Produto } from '../../models/produto.model';
+import {MatIconModule} from '@angular/material/icon';
 
 export interface PeriodicElement {
   name: string;
@@ -9,20 +18,63 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-];
-
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [MatTableModule,MatPaginatorModule],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule
+  ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
 export class ProdutosComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'nome', 'preco', 'dataDeCriacao', 'acoes'];
+
+  listaPaginada: ListaPaginada = {
+    itens: [],
+    page: 0,
+    total: 0,
+    totalPages: 5,
+  };
+
+  constructor (
+    private router: Router, 
+    private produtosService: ProdutosServiceService, 
+    private datePipe: DatePipe) 
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.produtosService.listarProdutos().subscribe(data => {
+      this.listaPaginada = <ListaPaginada>(data);
+    })
+  }
+
+  adicionarProduto() {
+    this.router.navigate(["/registro"])
+  }
+
+  editarProduto(item: Produto) {
+    this.router.navigate(["/registro"], { queryParams: { id: item.id } })
+  }
+
+  formatarDataEHora(data: string): string {
+    return this.datePipe.transform(data, 'dd/MM/yyyy HH:mm:ss') || '';
+  }
+
+  deletarProduto(item: Produto) {
+
+  }
+
+  visualizarProduto(item: Produto) {
+    this.router.navigate(["/detalhes"], { queryParams: { id: item.id } })
+  }
+
+
 }
